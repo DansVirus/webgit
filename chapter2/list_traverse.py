ef0043efad1054a985530ce2b3d268dddc9c7532
DansVirus <d.dans303@gmail.com>
2023-11-14 19:55:56 +0200

    chapter2 removal

--- chapter2/list_traverse.py
+++
@@ -1,6 +0,0 @@
-# List traverse
-
-fruits = ["apple", "banana", "melon", "anana"]
-
-for fruit in fruits:
-    print(fruit, end="\t")
--- chapter2/num_checker.py
+++
@@ -1,8 +0,0 @@
-num = -10
-
-if num < 0:
-    print("Negative number")
-elif num > 0:
-    print("Possitive number")
-else:
-    print("zero")
